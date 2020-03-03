157e5c1b8b1962933f5ad5262e49892041d4abed
Aiste Ilk <aistemariailk@gmail.com>
2020-03-03 12:02:34 +0100

    Zahlen initialisiert und eine Rechnung gemacht

--- src/HelloRainyDays.java
+++ src/HelloRainyDays.java
@@ -2,10 +2,13 @@ public class HelloRainyDays {
     public static void main (String[] args){
         System.out.print("Hello ");
         System.out.println("Campus");
+        int i = 2 ;
+        double d = 1.5;
+        double x =  i*d + d;
         System.out.println("It was a long rainy day when we first met");
         System.out.println("rain rain go away, come again another day");
         // TODO Klassen Namen Ausgabe einbauen
-        System.out.println("HelloRainyDays");
+        System.out.println("HelloRainyDays " +x);
         return;
         // Campus
         // HelloCampus
