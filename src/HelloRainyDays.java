19ed2160b9cd9c27ab17aeba9c8ef22515486be7
Aiste Ilk <aistemariailk@gmail.com>
2020-03-03 11:19:59 +0100

    3.3. 2020 heutige Arbeit

---
+++ src/HelloRainyDays.java
@@ -0,0 +1,14 @@
+public class HelloRainyDays {
+    public static void main (String[] args){
+        System.out.print("Hello ");
+        System.out.println("Campus");
+        System.out.println("It was a long rainy day when we first met");
+        System.out.println("rain rain go away, come again another day");
+        // TODO Klassen Namen Ausgabe einbauen
+        System.out.println("HelloRainyDays");
+        return;
+        // Campus
+        // HelloCampus
+
+    }
+}
